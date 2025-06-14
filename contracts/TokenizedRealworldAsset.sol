30a57c40bcd27cd962adf154c59dd9f217a07a53
hasuu011 <sgsuaisect@gmail.com>
2025-06-14 23:46:56 +0530

    Update TokenizedRealworldAsset.sol

--- contracts/TokenizedRealworldAsset.sol
+++ contracts/TokenizedRealworldAsset.sol
@@ -11,8 +11,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
  * Each token represents ownership or fractional ownership of a real-world asset
  */
 contract Project is ERC721, Ownable, ReentrancyGuard {
-    using Counters for Counters.Counter
-    Counters.Counter private _tokenI
+    
     struct Asset {
         string name;
         string description;
